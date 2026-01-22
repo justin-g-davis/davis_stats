0aff269f9c1c6596ed4ae78df042dc297a80b9bc
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2026-01-21 23:10:49 -0500

    Update __init__.py

--- davis_stats/__init__.py
+++ davis_stats/__init__.py
@@ -3,5 +3,6 @@ from .visualization.boxplot import boxplot
 from .visualization.histogram import histogram
 from .visualization.scatter import scatter
 from .data import (
-    ceo_comp)
+    ceo_comp, netflix_content, olympic_medals, 
+    restaurants, world_cup_goals)
 from .stats.reg import reg
