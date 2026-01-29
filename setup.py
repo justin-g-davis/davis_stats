48a8c713bebad1fbff212b37793504619083c7c4
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2026-01-29 12:16:24 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='davis_stats',
-    version='1.2',
+    version='1.3',
     packages=find_packages(),
     package_data={
         'davis_stats': ['data/*.xlsx']},
