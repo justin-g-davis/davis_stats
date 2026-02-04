4a74c979666e1b490ecd6b4ceab6c21643d61e14
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2026-02-04 13:36:12 -0500

    Update __init__.py

--- davis_stats/__init__.py
+++ davis_stats/__init__.py
@@ -8,6 +8,7 @@ from .reg_modeling.reg import reg
 
 from .reg_assumption_tests.bp_test import bp_test
 from .reg_assumption_tests.sw_test import sw_test
+from .reg_assumption_tests.dw_test import dw_test
 from .reg_assumption_tests.vif_test import vif_test
 
 from .datasets import (
