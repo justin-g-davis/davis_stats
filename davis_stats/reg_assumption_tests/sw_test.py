686183031711db7af6bbd2fc36fab847a35ec23e
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2026-02-04 12:36:45 -0500

    Update sw_test.py

--- davis_stats/reg_assumption_tests/sw_test.py
+++ davis_stats/reg_assumption_tests/sw_test.py
@@ -18,7 +18,7 @@ def sw_test(df, y, x, dummies=None, logistic=False):
     
     sw_stat, sw_pvalue = stats.shapiro(residuals)
     
-    print("If p-value ≤ 0.05: Reject H0, assume non-normality. Further remedies may be needed but less critical with a large sample size.")
+    print("If p-value ≤ 0.05: Reject H0, assume non-normality. Remedies may be needed, but less critical with a large sample size.")
     print("If p-value > 0.05: Fail to reject H0, assume normality.\n")
     print(f"Sample size: {n}")
     print(f"p-value: {sw_pvalue:.4f}")
