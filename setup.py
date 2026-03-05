1949dbf864251afd209d5cdfb1beef1d273231a5
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2026-03-05 09:30:29 -0500

    Bump version from 1.7 to 1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='davis_stats',
-    version='1.7',
+    version='1.8',
     packages=find_packages(),
     package_data={
         'davis_stats': ['datasets/*.xlsx']},
