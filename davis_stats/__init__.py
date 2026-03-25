0cbab84f19bd61947ec95dd46556a21f87761735
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2026-03-25 14:56:01 -0400

    Update import paths for regression assumption tests

--- davis_stats/__init__.py
+++ davis_stats/__init__.py
@@ -7,10 +7,10 @@ from .visualization.residuals import residuals
 from .reg_modeling.reg import reg
 from .reg_modeling.marginal_effects import marginal_effects
 
-from .reg_assumption_tests.bp_test import bp_test
-from .reg_assumption_tests.sw_test import sw_test
-from .reg_assumption_tests.dw_test import dw_test
-from .reg_assumption_tests.vif_test import vif_test
+from .reg_tests.bp_test import bp_test
+from .reg_tests.sw_test import sw_test
+from .reg_tests.dw_test import dw_test
+from .reg_tests.vif_test import vif_test
 
 from .datasets import (
     ceo_comp, netflix_content, olympic_medals, 
