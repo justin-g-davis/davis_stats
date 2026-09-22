945ec3bd9595fb0229348c52528c50fee2d59424
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2026-09-22 15:07:24 -0400

    Update version number to 2.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='davis_stats',
-    version='2.2',
+    version='2.1.2',
     packages=find_packages(),
     package_data={
         'davis_stats': ['datasets/*.xlsx']},
